a5e10536ba992c611de01f13ce26f18b16510c14
Zygimantus <zygimantus@gmail.com>
2017-04-23 23:44:02 +0300

    Migration to single index.html file

--- src/main/java/com/zygimantus/marvelis/DispatcherConfig.java
+++ src/main/java/com/zygimantus/marvelis/DispatcherConfig.java
@@ -1,22 +1,10 @@
 package com.zygimantus.marvelis;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 /**
  *
@@ -30,41 +18,8 @@ public class DispatcherConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+        registry.addResourceHandler("/index.html").addResourceLocations("/index.html");
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-
-    }
-
-    @Bean(name = "jspViewResolver")
-    public ViewResolver getJspViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        resolver.setContentType("text/html;charset=UTF-8");
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-        // stringhttp
-        HttpMessageConverter<?> converter = new StringHttpMessageConverter();
-        ((StringHttpMessageConverter) converter).setSupportedMediaTypes(
-                Arrays.asList(new MediaType("text", "plain", StandardCharsets.UTF_8)));
-
-        converters.add(converter);
-
-        // form
-        converter = new FormHttpMessageConverter();
-        converters.add(converter);
-
-        // json
-//        converter = new MappingJackson2HttpMessageConverter();
-//        converters.add(converter);
     }
 
 }
